9fabc43258f7bb7e81f2586dbcf8096848310f1c
Aditi-panta <88805173+Aditi-panta@users.noreply.github.com>
2023-01-21 13:52:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+function preload(){
+    clown_nose=loadImage("https://o.remove.bg/downloads/0322a141-272b-42a2-ac7a-c57222cb5024/360_F_231423149_ldug4Sv3LV2FBLGt5sAQ91c2iYvntCXU-removebg-preview.png");
+}
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
@@ -14,12 +19,13 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length>0){
     console.log(results);
-    noseX=results[0].pose.nose.x;
-    noseY=results[0].pose.nose.y;
+    noseX=results[0].pose.nose.x-30;
+    noseY=results[0].pose.nose.y+5;
 }
 }
 function draw(){
     image(video,0,0,300,300);
+    image(clown_nose,noseX,noseY,70,30);
 }
 function take_snapshot(){
     save("myfilterimage.png");
